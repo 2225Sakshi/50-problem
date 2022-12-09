fb1b681860c53a7b2f48dbcc1d17debfa836a800
Algo Sakshi <112638074+2225Sakshi@users.noreply.github.com>
2022-12-09 18:46:24 +0530

    Added solution - LeetHub

---
+++ Black and White - GFG/black-and-white.py
@@ -0,0 +1,56 @@
+#User function Template for python3
+
+
+#Function to find out the number of ways we can place a black and a white
+#Knight on this chessboard such that they cannot attack each other.
+def numOfWays(M, N):
+    ans=0
+    squares=N*M
+    for i in range(N):
+        for j in range(M):
+            curr=squares-1
+            if -1<i-2<N:
+                if -1<j-1<M:
+                    curr-=1
+                if -1<j+1<M:
+                    curr-=1
+            if -1<i-1<N:
+                if -1<j-2<M:
+                    curr-=1
+                if -1<j+2<M:
+                    curr-=1
+            if -1<i+1<N:
+                if -1<j-2<M:
+                    curr-=1
+                if -1<j+2<M:
+                    curr-=1
+            if -1<i+2<N:
+                if -1<j-1<M:
+                    curr-=1
+                if -1<j+1<M:
+                    curr-=1
+            ans=(ans+curr)%(10**9+7)
+    return ans
+            
+    
+        
+    # code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        m,n = map(int,input().strip().split())
+        print(numOfWays(m,n))
+
+# } Driver Code Ends
